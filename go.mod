60b203f2a8e0cdc2470bbf83304e859be7e45261
Julian Zhao <vastscheme@gmail.com>
2024-06-04 15:03:45 +0800

    add stringutil reverse

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JulianZhao/example
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/JulianZhao/example/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello"))
+}
